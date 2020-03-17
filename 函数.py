2f28288a46adc8ac14868dde6ca9a0126c6a8c06
HwiLu <2418979865@qq.com>
2020-03-17 12:24:19 +0800

    Upodate

--- 函数.py
+++ 函数.py
@@ -24,3 +24,63 @@ print(result)
 print('Lucy','Kafka','Mike')
 
 print('Lucy','Kafka','Mike',end='.',sep=',')
+
+
+'''
+异常处理
+可能出错的语句放在try语句中，如果错误发生，程序执行就立即转到接下来的except子句开始处，不会执行try中剩下的语句。
+
+'''
+def spam(divideBy):
+	try:
+		return 42 / divideBy
+	except ZeroDivisionError:
+		print('Error:Invalid argument')
+		
+print(spam(2))
+print(spam(0))
+		
+	
+	
+
+
+'''一个小程序'''
+import random
+secretNumber = random.randint(1,20)
+print("Input number between 1-20")
+# 猜6次
+for guessesTaken in range(1,7):
+	print("Take a guess")
+	guess = int(input())
+	if guess < secretNumber:
+		print("You guess is too low")
+
+	if guess > secretNumber:
+		print("You guess is too high")
+	else:
+		break # 正确猜中
+if guess == secretNumber:
+	print("Good job, bingo.")
+else:
+	print("You did not guess right after 6 times")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
